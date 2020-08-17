07b97aead6a5f6000f0ad7f8ec0ccef12b7e67c2
kim kiamco <kiamcoo@gmail.com>
2020-08-16 18:27:38 -0700

    Add files via upload

---
+++ create_cover_letter.py
@@ -0,0 +1,28 @@
+
+import click
+
+@click.command()
+@click.option("-c","--company", help="company name" )
+@click.option("-j","--job", help="job title")
+@click.option("-f","--filetype", default="txt",help="ie: txt, docx, doc")
+def cli(company, job, filetype):
+    cover_template = f""" 
+Hi,
+
+    My name is Kim Kiamco,  I am excited for the opportunity to apply for the {job} position. I will be an asset to the team because of my commitment to building quality web applications. I am a part-time student at coding bootcamp (Lambda school) taking the Full-Stack Web Development track and will be finishing the program soon. Currently, I work as a part time Operations engineer at Menlo Security, a cyber security company. I have been looking for a full time position and thought {company} would be a great company to work for. My skills from Lambda school, working experience from Menlo Security and my adaptability in any environment will allow me to excel at {company}. My main tech stack when working on web applications is Javascript, React, Node.js, and PostgreSQL but I am willing and excited to learn new languages if needed. I am familiar with DevOps tools and practices like Docker, AWS and can write python scripts that can help with deployment automation.
+ 
+    I have attached my resume and I believe that my experience and education would make me an asset at {company},  I look forward to contributing to a great company. I would love to talk more about it, please contact me at kiamcoo@gmail.com or call (909)-767-5806. You can also check out my portfolio at https://www.kiamco.dev/. Thank you for taking the time to read this, I hope to hear from you soon.   
+ 
+Thanks, 
+Kim Kiamco
+
+    """
+    # create file 
+    file = open(f'/cover_letters/cover_letter_for_{company}.{filetype}','w')
+    file.write(cover_template)
+
+    print("template created")
+    print(cover_template)
+
+if __name__ == "__main__":
+    cli()
